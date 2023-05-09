f1eac239581e59f3afaddff2a3f8a76feabe2dd3
Kyle Erhabor <kyleerhabor@gmail.com>
2023-05-09 17:58:23 -0400

    Make nonce generators public

--- Sources/Nonce.swift
+++ Sources/Nonce.swift
@@ -1,10 +1,10 @@
 import Foundation
 
-func generateNonce(async: Bool = false) -> String {
+public func generateNonce(async: Bool = false) -> String {
     return "\(async ? "async" : "sync");\(UUID().uuidString)"
 }
 
-func isNonceAsync(nonce: String) throws -> Bool {
+public func isNonceAsync(nonce: String) throws -> Bool {
     let components = nonce.components(separatedBy: ";")
     if components.count != 2 || (components[0] != "async" && components[0] != "sync") {
         throw NonceError.invalid(nonce: nonce)
